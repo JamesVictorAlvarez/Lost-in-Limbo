644f8f391b49ffb54ba55220d2bbfabbdb7c5687
James Victor Alvarez <2182866@edu.vaniercollege.qc.ca>
2023-10-12 11:20:57 -0400

    kinda fixed the flashlight

--- Assets/Script/Flashlight.cs
+++ Assets/Script/Flashlight.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
+    /*
     private Vector3 vector;
     public float speed = 5.0f;
     public GameObject gameObject;
+    */
+    public GameObject light;
+    private bool isLightOn = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        //gameObject = Camera.main.gameObject;
+        /*
+        gameObject = Camera.main.gameObject;
         // Getting the offset of the postion
         vector = transform.position - gameObject.transform.position;
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         transform.position = gameObject.transform.position + vector;
         transform.rotation = Quaternion.Slerp(transform.rotation, gameObject.transform.rotation, speed * Time.deltaTime);
+        */
+        if (Input.GetKeyDown("f"))
+        {
+            isLightOn = !isLightOn;
+            light.SetActive(isLightOn);
+        }
+        
     }
 }
