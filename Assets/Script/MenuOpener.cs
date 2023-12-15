692c53ed3c03b92d00f5ba4e6ed2c2ac5403a083
c-ncl <89605841+c-ncl@users.noreply.github.com>
2023-12-15 17:19:43 -0500

    Made the Pause Menu.

---
+++ Assets/Script/MenuOpener.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOpener : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.gameObject.SetActive(!pauseMenu.gameObject.activeSelf);
+        }
+
+        if(pauseMenu.gameObject.activeSelf)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        } 
+        else 
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+
+    }
+}
